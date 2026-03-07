c2b7fecc1367324262e9d4249056fe0cf67b494e
Shah Zeb <zebshah7851@gmail.com>
2026-03-07 17:40:10 +0500

    first commit

---
+++ helpers/_replicate.py
@@ -0,0 +1,45 @@
+from functools import lru_cache
+
+from decouple import config
+from replicate.client import Client
+
+REPLICATE_API_TOKEN = config("REPLICATE_API_TOKEN")
+REPLICATE_MODEL = config("REPLICATE_MODEL")
+REPLICATE_MODEL_VERSION = config("REPLICATE_MODEL_VERSION")
+
+@lru_cache
+def get_replicate_client():
+    return Client(api_token=REPLICATE_API_TOKEN)
+
+@lru_cache
+def get_replicate_model_version():
+    replicate_client = get_replicate_client()
+    rep_model = replicate_client.models.get(REPLICATE_MODEL)
+    rep_version = rep_model.versions.get(REPLICATE_MODEL_VERSION)
+    return rep_version
+
+def generate_image(prompt: str, 
+                    model: str = REPLICATE_MODEL,
+                    model_version: str = REPLICATE_MODEL_VERSION,
+                    num_outputs: int = 2, 
+                    output_format: str = "jpg",
+                    enrich_trigger_word: bool = False, trigger_word: str = "TOK"):
+    if enrich_trigger_word:
+        if trigger_word not in prompt:
+            raise ValueError(f"Trigger word {trigger_word} not found in prompt")
+    input_args = {
+        "prompt": prompt,
+        "num_outputs": 2,
+        "output_format": "jpg",
+    }
+    replicate_client = get_replicate_client()
+    rep_version = get_replicate_model_version()
+
+    pred = replicate_client.predictions.create(
+                            version=rep_version,
+                            input=input_args
+                        )
+    return {
+        "id": pred.id,
+        "status": pred.status,
+    }
---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+from decouple import config
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import helpers
+
+REDIS_URL = config("REDIS_URL")
+
+app = FastAPI()
+
+@app.get("/")
+def read_index():
+    # helpers.generate_image()
+    return {"message": "Hello World"}
+
+class ImageGenerationRequest(BaseModel):
+    prompt: str
+
+@app.post("/generate")
+def create_image(data: ImageGenerationRequest):
+    try:
+        pred_result = helpers.generate_image(data.prompt)
+        return pred_result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
