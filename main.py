20c9be2b0a5edde0df0318334a606d3ee0c8cbd4
Shah Zeb <zebshah7851@gmail.com>
2026-03-07 23:26:35 +0500

    Enhance main.py with Redis integration and rate limiting. Updated root endpoint to include rate limiting dependencies and modified response format.

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
+import redis.asyncio as redis
+
+from contextlib import asynccontextmanager
 from typing import Union
 
 from decouple import config
-from fastapi import FastAPI, HTTPException
+from fastapi import (
+    FastAPI,
+    Depends,
+    HTTPException)
+from pyrate_limiter import Duration, Limiter, Rate
+from fastapi_limiter.depends import RateLimiter
+
 from pydantic import BaseModel
+
 import helpers
 
 REDIS_URL = config("REDIS_URL")
 
 app = FastAPI()
 
-@app.get("/")
-def read_index():
+@app.get("/",
+    dependencies=[
+        Depends(RateLimiter(limiter=Limiter(Rate(2, Duration.SECOND*5)))),
+        Depends(RateLimiter(limiter=Limiter(Rate(10, Duration.MINUTE*1))))
+    ]
+)
+def read_root():
     # helpers.generate_image()
-    return {"message": "Hello World"}
+    return {"Hello": "World"}
 
 class ImageGenerationRequest(BaseModel):
     prompt: str
